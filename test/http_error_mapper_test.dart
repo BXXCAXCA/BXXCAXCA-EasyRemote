0f722345595df957b049b212a4a855e7a71ad567
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-27 22:56:09 +0800

    Expand http error mapper tests

--- test/http_error_mapper_test.dart
+++ test/http_error_mapper_test.dart
@@ -4,8 +4,23 @@ import 'package:easyremote/src/models/http_request_descriptor.dart';
 import 'package:easyremote/src/services/http_error_mapper.dart';
 
 void main() {
+  test('maps auth status', () {
+    const response = HttpResponseDescriptor(statusCode: 401, headers: {}, body: '');
+    expect(HttpErrorMapper().map(response), AppErrorCode.authRequired);
+  });
+
   test('maps not found status', () {
     const response = HttpResponseDescriptor(statusCode: 404, headers: {}, body: '');
     expect(HttpErrorMapper().map(response), AppErrorCode.remoteNotFound);
+  });
+
+  test('maps conflict status', () {
+    const response = HttpResponseDescriptor(statusCode: 409, headers: {}, body: '');
+    expect(HttpErrorMapper().map(response), AppErrorCode.conflictDetected);
+  });
+
+  test('maps server status', () {
+    const response = HttpResponseDescriptor(statusCode: 503, headers: {}, body: '');
+    expect(HttpErrorMapper().map(response), AppErrorCode.networkUnavailable);
   });
 }
