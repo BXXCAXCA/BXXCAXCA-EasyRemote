80bc32e4a4efbca2e4027cea52009d4e3048cf2f
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-27 18:22:13 +0800

    Add state barrel file

---
+++ lib/src/state/state.dart
@@ -0,0 +1,6 @@
+export 'activity_history_controller.dart';
+export 'adapter_registry_controller.dart';
+export 'inventory_controller.dart';
+export 'settings_controller.dart';
+export 'sync_controller.dart';
+export 'vault_controller.dart';
